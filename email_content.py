0765069ef4413c8b9f2b3af371a08c9b5a9ca19c
Stanislaw Dutkiewicz <stan.dutkiewicz@hotmail.com>
2024-06-23 10:34:32 +0200

    fixing errors

--- email_content.py
+++ email_content.py
@@ -1,8 +1,16 @@
+import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from io import BytesIO
 import requests
+
+def download_image(url):
+    if not url.startswith(('http://', 'https://')):
+        url = 'https:' + url
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.content
 
 def create_email_content(popular_news, newest_news):
     email_content = """
@@ -48,23 +56,26 @@ def create_email_content(popular_news, newest_news):
     return email_content
 
 def create_email_message(email_content, popular_news, newest_news, sender_email, recipient_emails):
-    msg = MIMEMultipart('related')
+    msg = MIMEMultipart('alternative')
     msg['Subject'] = "Today's Top News"
     msg['From'] = sender_email
-    msg['To'] = ", ".join(recipient_emails)
+    msg['To'] = ", ".join(recipient_emails.split(','))
 
     part = MIMEText(email_content, 'html')
     msg.attach(part)
 
-    def download_image(url):
-        response = requests.get(url)
-        return BytesIO(response.content)
-
+    # Attach images
     for article in popular_news + newest_news:
         if article['img_src']:
             img_data = download_image(article['img_src'])
-            image = MIMEImage(img_data.read())
+            image = MIMEImage(img_data)
             image.add_header('Content-ID', f"<{article['img_src'].split('/')[-1]}>")
             msg.attach(image)
 
     return msg
+
+def send_email(msg, sender_email, sender_password, recipient_emails):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.login(sender_email, sender_password)
+        smtp.sendmail(sender_email, recipient_emails.split(','), msg.as_string())
