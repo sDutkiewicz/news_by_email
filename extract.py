32da1e652d77e877feb72dc81b90f12aa899fa85
Stanislaw Dutkiewicz <stan.dutkiewicz@hotmail.com>
2024-06-27 21:18:07 +0200

    handle errors

--- extract.py
+++ extract.py
@@ -20,17 +20,32 @@ def extract_info(soup, section_class, article_selector, title_selector, exclude_
         main_photo = linked_soup.find('figure', class_='mainPhoto')
         
         lead_text = lead_div.get_text(strip=True) if lead_div else "No description available"
-        img_src = main_photo.find('img')['src'] if main_photo and main_photo.find('img') else None
-        img_alt = main_photo.find('img').get('alt', 'No description available') if img_src else "No description available"
-        img_name = img_src.split('/')[-1] if img_src else "no_image"
+        
+        img_src = None
+        img_alt = "No description available"
+        img_name = "no_image"
+
+        if main_photo and main_photo.find('img'):
+            img_tag = main_photo.find('img')
+            img_src = img_tag.get('src', None)
+            img_alt = img_tag.get('alt', 'No description available')
+            img_name = img_src.split('/')[-1] if img_src else "no_image"
+            if img_src and img_src.startswith("//"):
+                img_src = f"https:{img_src}"
 
         articles.append({
             'title': title,
             'lead': lead_text,
-            'img_src': f"https:{img_src}" if img_src.startswith("//") else img_src,
+            'img_src': img_src,
             'img_alt': img_alt,
             'img_name': img_name,
             'link': link
         })
 
     return articles
+
+# Example usage
+url = 'https://example.com/popular-news'
+soup = fetch_and_parse(url)
+popular_news = extract_info(soup, 'section.widgetFeed', 'li a', 'div.popularNewsTitle')
+print(popular_news)
